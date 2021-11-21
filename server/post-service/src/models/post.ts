84951a367011f6def67016b7a1f7f3262ccb69ef
Rinkle Rani <rinkle.aps@gmail.com>
2021-11-20 21:20:24 -0800

    1. Added the model for Post service 2. Added .gitignore files for the whole project

---
+++ server/post-service/src/models/post.ts
@@ -0,0 +1,13 @@
+class Post {
+    postId: string;
+    title: string;
+    price: number;
+    category: string;
+    condition: string;
+    description: string;
+
+    constructor(postId: string){
+        this.postId = postId;
+    }
+
+}
