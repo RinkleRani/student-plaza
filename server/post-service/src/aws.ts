1ad0cf062cb3498dc471782948719e6b6156756d
Rinkle Rani <rinkle.aps@gmail.com>
2021-11-28 04:02:37 -0800

    Added logs

--- server/post-service/src/aws.ts
+++ server/post-service/src/aws.ts
@@ -1,9 +1,14 @@
 import AWS = require('aws-sdk');
+import { Logger } from 'tslog';
 import { config } from "./config";
 
+var logger = new Logger();
 
 const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
 AWS.config.credentials = credentials
+logger.debug("AWS CREDENTAILS: " + credentials)
+logger.debug("AWS PROFILE: " + config.aws_profile)
+logger.debug("AWS REGION: " + config.aws_region)
 
 export const documentClient = new AWS.DynamoDB.DocumentClient({
     region: config.aws_region
