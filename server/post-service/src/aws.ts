f65424f6ba9006b5e52a1315825b9f56180a4617
Rinkle Rani <rinkle.aps@gmail.com>
2021-11-28 23:43:10 -0800

    Set the aws environment variables

--- server/post-service/src/aws.ts
+++ server/post-service/src/aws.ts
@@ -2,13 +2,15 @@ import AWS = require('aws-sdk');
 import { Logger } from 'tslog';
 import { config } from "./config";
 
-var logger = new Logger();
 
-const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
-AWS.config.credentials = credentials
-logger.debug("AWS CREDENTAILS: " + credentials)
+var logger = new Logger();
+logger.debug("AWS KEY: " + config.aws_key_id.substr(config.aws_key_id.length - 4))
+logger.debug("AWS SECRET: " + config.secret_access_key.substr(config.secret_access_key.length - 4))
 logger.debug("AWS PROFILE: " + config.aws_profile)
 logger.debug("AWS REGION: " + config.aws_region)
+
+AWS.config.accessKeyId = config.aws_key_id
+AWS.config.secretAccessKey = config.secret_access_key
 
 export const documentClient = new AWS.DynamoDB.DocumentClient({
     region: config.aws_region
